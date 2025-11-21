abfd19c5a7adac4c0475e56f54411d24deef30f9
Manan Babbar <mananbabbar20@gmail.com>
2025-11-21 20:00:47 +0530

    Add files via upload

---
+++ BASICS OF C++ DAY 2/ageindays.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"Enter your age in years: ";
+    cin>>age;
+    int x = age*365;
+    cout<<"Entered age in days will be: "<<x;
+    return 0;
+}
